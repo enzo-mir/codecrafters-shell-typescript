3e5f6da705a4be980817bd538bc49fbd4eecece1
Lagacy_code <miraglioenzo93@gmail.com>
2025-05-04 14:48:50 +0200

    [any message]

--- app/main.ts
+++ app/main.ts
@@ -5,17 +5,32 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+const acceptedCommands = ["echo", "type", "exit"];
+
 const promt = () => {
   rl.question("$ ", (answer) => {
     if (answer === "exit 0") {
       rl.close();
       return;
     }
-    if(answer.includes("echo")) {
+    if (answer.includes("echo")) {
       const echo = answer.split("echo ")[1];
       console.log(echo);
       promt();
       return;
+    }
+    if (answer.includes("type")) {
+      const type = answer.split("type ")[1];
+
+      if (acceptedCommands.includes(type)) {
+        console.log(`${type} is a shell builtin`);
+        promt();
+        return;
+      } else {
+        console.log(`${type}: not found`);
+        promt();
+        return;
+      }
     }
     console.log(`${answer}: command not found`);
     promt();
